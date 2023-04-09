3e0df8e0221faed2d933b4a1b0382a516c75a814
Lydmila Kismatulina <tenzinwangmo@yandex.ru>
2023-04-09 21:42:58 +0300

    структуризирую папки

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+bool IsTriangleExistent(int a, int b, int c)
+{
+    if (a + b < c);
+
+
+}
+
